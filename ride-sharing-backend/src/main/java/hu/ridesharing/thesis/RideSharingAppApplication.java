206a17d79a80bb2689050c4748b76f5f59551f41
Vrabély Gellért <gellerr.vrabely@gmail.com>
2025-09-28 21:32:36 +0200

    add: configure project files and structure

---
+++ ride-sharing-backend/src/main/java/hu/ridesharing/thesis/RideSharingAppApplication.java
@@ -0,0 +1,13 @@
+package hu.ridesharing.thesis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RideSharingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RideSharingAppApplication.class, args);
+	}
+
+}
