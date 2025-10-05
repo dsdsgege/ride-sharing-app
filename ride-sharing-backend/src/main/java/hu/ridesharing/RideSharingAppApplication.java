3557dffbc353aa087b9ea6c25480db3ff9754f0f
Gellért Vrabély <gellerr.vrabely@gmail.com>
2025-10-05 19:58:16 +0200

    add: add landing page and correct docker-compose

--- ride-sharing-backend/src/main/java/hu/ridesharing/RideSharingAppApplication.java
+++ ride-sharing-backend/src/main/java/hu/ridesharing/RideSharingAppApplication.java
@@ -1,4 +1,4 @@
-package hu.ridesharing.thesis;
+package hu.ridesharing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ ride-sharing-frontend/tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  darkMode: 'class',
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#13a4ec",
+        "background-light": "#f6f7f8",
+        "background-dark": "#101c22",
+      },
+      fontFamily: {
+        display: ["Plus Jakarta Sans", "sans-serif"],
+      },
+    },
+  },
+  plugins: [
+  ],
+}
